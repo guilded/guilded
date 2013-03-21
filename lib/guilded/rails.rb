34be4555ad5746799679e6faca45104152085187
Jason Harrelson <jason@lookforwardenterprises.com>
2013-03-21 16:00:03 -0500

    stashing

--- lib/guilded/rails.rb
+++ lib/guilded/rails.rb
@@ -1,6 +1,7 @@
 module Guilded::Rails
 
   autoload :Controller,  'guilded/rails/controller'
+  autoload :Helpers,     'guilded/rails/helpers'
   autoload :ViewHelpers, 'guilded/rails/view_helpers'
 
 end
